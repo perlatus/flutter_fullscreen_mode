0c51a199f4c2caa3725cb872c1392253fe230beb
perlatus <perlatus@git.email.vczf.io>
2017-05-17 03:00:37 -0400

    Write functional plugin
    However, it doesn't stay sticky unless it's KitKat or higher, which
    isn't that great. Might need to implement a workaround for older
    devices.

--- android/src/main/java/com/yourcompany/fullscreen_mode/FullscreenModePlugin.java
+++ android/src/main/java/com/yourcompany/fullscreen_mode/FullscreenModePlugin.java
@@ -14,34 +14,35 @@ import android.view.View;
  * FullscreenModePlugin
  */
 public class FullscreenModePlugin implements MethodCallHandler {
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "fullscreen_mode");
-    channel.setMethodCallHandler(new FullscreenModePlugin((FlutterActivity)registrar.activity()));
-  }
-
-  private final FlutterActivity activity;
-
-  private FullscreenModePlugin(FlutterActivity activity) {
-    this.activity = activity;
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      activity.getFlutterView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-          );
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "fullscreen_mode");
+        channel.setMethodCallHandler(new FullscreenModePlugin((FlutterActivity)registrar.activity()));
     }
-  }
+
+    private final FlutterActivity activity;
+
+    private FullscreenModePlugin(FlutterActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("setFullscreen")) {
+            activity.getFlutterView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        } else if (call.method.equals("setNormal")) {
+            activity.getFlutterView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        } else {
+            result.notImplemented();
+        }
+    }
 }
--- lib/fullscreen_mode.dart
+++ lib/fullscreen_mode.dart
@@ -6,6 +6,11 @@ class FullscreenMode {
   static const MethodChannel _channel =
       const MethodChannel('fullscreen_mode');
 
-  static Future<String> get platformVersion =>
-      _channel.invokeMethod('getPlatformVersion');
+  static void setFullscreen() {
+    _channel.invokeMethod('setFullscreen');
+  }
+
+  static void setNormal() {
+    _channel.invokeMethod('setNormal');
+  }
 }
